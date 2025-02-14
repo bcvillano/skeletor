ec945d387e71d10daca80f11a1ebbeb056b5fc0d
bvillano05 <bvillano05@gmail.com>
2025-02-14 15:24:48 -0500

    command execution works in test

--- client/go/main.go
+++ client/go/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	//"net/http"
-	//"os/exec"
+	"os/exec"
 	"net"
 	"time"
+	"runtime"
 )
 
 var host string = "localhost"
 var port int = 80
+
+func runcmd(command string) ([]byte, error) {
+	if runtime.GOOS == "windows" {
+		return exec.Command("powershell", "/c", command).CombinedOutput()
+	} else {
+		return exec.Command("/bin/bash", "-c", command).CombinedOutput()
+	}
+}
 
 func main() {
 	server := "http://" + net.JoinHostPort(host, fmt.Sprintf("%d", port))
@@ -17,6 +26,12 @@ func main() {
 	for true{
 		fmt.Println("Counter: ", counter)
 		fmt.Println("URL: ", server )
+		output,err := runcmd("whoami; hostname")
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}else{
+			fmt.Println("Output: ", string(output))
+		}
 		counter++
 		time.Sleep(1 * time.Second)
 	}
