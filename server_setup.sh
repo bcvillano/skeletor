98d4ee17b16058b0890121e161cab38b5102a504
bvillano05 <bvillano05@gmail.com>
2025-04-28 18:21:13 -0400

    Modified setup script
    Also prep for changes to targeter.py script

--- server_setup.sh
+++ server_setup.sh
@@ -6,17 +6,18 @@ if [ "$EUID" -ne 0 ]
 fi
 
 echo "Moving Skeletor files to /usr/local/bin..."
-mv ./skelctl.py /usr/local/bin/skelctl
+cp ./skelctl.py /usr/local/bin/skelctl
 chmod 755 /usr/local/bin/skelctl
 chown root:root /usr/local/bin/skelctl
 
-mv ./skeletor.py /usr/local/bin/skeletor
+cp ./skeletor.py /usr/local/bin/skeletor
 chmod 755 /usr/local/bin/skeletor
 chown root:root /usr/local/bin/skeletor
 
 echo "Setting Up Skeletor Service..."
-mv ./skeletor.service /etc/systemd/system/skeletor.service
+cp ./skeletor.service /etc/systemd/system/skeletor.service
 systemctl daemon-reload
 #systemctl enable skeletor
 
 echo "Skeletor installed successfully"
+echo "To run Skeletor as a service, use the command: systemctl start skeletor"
--- targeter.py
+++ targeter.py
@@ -18,21 +18,41 @@ def template(template_str,num_teams): #formats a template string into a list of 
     target_str = target_str[:-1] #remove last comma
     return target_str
 
+def ansible_ini():
+    pass
+
+def ansible_yaml():
+    pass
+
 def main():
     print("Skeletor Targeter\n")
-    print("1. All targets")
-    print("2. Template String (Ex: 10.10.x.1)")
-    print("3. Exit")
+    print("1. Template String (Ex: 10.10.x.1)")
+    print("2. All targets")
+    print("3. Ansible Inventory Groups")
+    print("4. Exit")
 
     mode = input("Mode: ").strip()
     match mode:
         case "1":
-            print(all_targets())
-        case "2":
             template_str = input("Template String (format 10.x.10.1): ").strip()
             num_teams = int(input("Number of teams: ").strip())
             print(template(template_str,num_teams))
+        case "2":
+            print(all_targets())
         case "3":
+            print("Ansible Inventory File Format:")
+            print("1. .ini\n2. .yaml\n3. Cancel")
+            userin = input(": ")
+            if userin.upper().strip() in ["1",".INI","INI"]:
+                ansible_ini()
+            elif userin.upper().strip() in ["2",".YAML","YAML"]:
+                ansible_yaml()
+            elif userin.upper().strip() == "3":
+                main()
+            else:
+                print("Invalid Option")
+                return
+        case "4":
             return
         case _:
             print("Invalid mode")
