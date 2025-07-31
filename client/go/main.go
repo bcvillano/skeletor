c6d50c2004d737ef792880286d5337a5c2b8eafa
bvillano05 <bvillano05@gmail.com>
2025-07-31 16:26:56 -0400

    Created structs
    Task struct for task received
    Result struct for result being returned to server

--- client/go/main.go
+++ client/go/main.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 )
 
+type Task struct {
+	Action string `json:"action"`
+	Command string `json:"command"`
+	Filename string `json:"filename"`
+	TaskID string `json:"task_id"`
+}
+
+type Result struct {
+	AgentID string `json:"agent_id"`
+	TaskID string `json:"task_id"`
+	Result string `json:"result"`
+	ReturnCode int `json:"returncode"`
+}
+
 func main(){
 	fmt.Println("Hello, World!")
 	fmt.Println("I'll finish this client later.")
